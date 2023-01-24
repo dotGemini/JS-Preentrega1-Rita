ac159771809f8f455de7c48bbb8248218e62ad48
Diego Rita <diegohr91@gmail.com>
2023-01-24 18:56:14 -0300

    segunda preentrega

--- js/script.js
+++ js/script.js
@@ -1,25 +1,71 @@
+let running = true;
+let Memoria = {
+    operation: [],
+    result: 0
+};
+
 function Calculadora (a, b, op) {
+
+    Memoria.operation.push(op)
+    if (b !== null) {
+        Memoria.operation.push(b)
+    }
 
     switch (op){
 
         case '+':
-            return a+b;
+            Memoria.result = a+b;
+            break
         
         case '-':
-            return a-b;
-            break;
+            Memoria.result = a-b;
+            break
 
         case '/':
-            return a/b;
-            break;
+            Memoria.result = a/b;
+            break
 
         case '*':
-            return a*b;
-            break;
+            Memoria.result = a*b;
+            break
+
+        case '=':
+            running = false;
+            return Memoria.result; 
     }
 }
 
-let a = Number(prompt ("Ingrese un numero"));
-op = prompt ("Ingrese un operador valido: + - / *");
-let b = Number(prompt ("Ingrese otro numero"));
-alert ("Resultado: " + Calculadora(a, b, op))
+function Program(){
+
+    let a = CheckValue(prompt ("Ingrese un numero"));
+    Memoria.result = a;
+    Memoria.operation.push(a)
+
+    while (running) {
+        let op = prompt ("Ingrese un operador valido: + - / *");
+        if (op === "=") {
+            alert ("resultado: " + Calculadora(Memoria.result, null, op))
+            Memoria.operation.map((coso) => console.log(coso))
+            console.log(Memoria);
+            break
+        }
+        let b = CheckValue(prompt ("Ingrese otro numero"));
+        Calculadora(Memoria.result, b, op)
+    }
+}
+
+function CheckValue(a) {
+    if (isNaN(a)) {
+        if(a === "=")
+            running = false;
+        else{
+            alert("Solo son validos valores numericos para operar")
+        }
+        return false;
+    }
+    return Number(a);
+}
+
+Program();
+
+
